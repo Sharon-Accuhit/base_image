ab55d4513d5fc15d34856e791fd29d7b1f7ae0a3
Sharon-Accuhit <sharon.chen@accuhit.net>
2023-02-16 16:51:47 +0800

    修改 codedeploy/container.sh 的註解

--- codedeploy/container.sh
+++ codedeploy/container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#workdir
+# workdir
 cd app
 
-#install docker
+# install docker
 docker &>/dev/null
 [ $? = 0 ] && yum erase docker -y 
 yum upgrade && yum update -y
@@ -20,10 +20,10 @@ rm -r aws
 hash -r
 /usr/local/bin/aws --version
 
-#set variable
+# set variable
 source config.txt
 
-#remove the previous images
+# remove the previous images
 docker images | grep "public.ecr.aws/$ALIAS/$IMAGE_REPO_NAME" &>/dev/null
 if [ $? = 0 ]
 then
@@ -36,7 +36,7 @@ then
   rm tag.txt
 fi
 
-#pull image
+# pull image
 echo Logging in to Amazon ECR...
 /usr/local/bin/aws ecr-public get-login-password --region $AWS_DEFAULT_REGION | \
 docker login --username AWS --password-stdin \
@@ -44,16 +44,16 @@ public.ecr.aws/$ALIAS
 echo Pulling the Docker image...
 docker pull public.ecr.aws/$ALIAS/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-#run container
+# run container
 docker ps -a | grep $CONTAINER &>/dev/null
 [ $? = 0 ] && docker rm -f $CONTAINER
 docker run --name=$CONTAINER --restart=always -itd \
 public.ecr.aws/$ALIAS/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-#show the container is successfully running
+# show the container is successfully running
 docker logs $CONTAINER 
 
-#remove the previous images
+# remove the previous images
 docker images | grep "none" &>/dev/null
 if [ $? = 0 ]
 then
