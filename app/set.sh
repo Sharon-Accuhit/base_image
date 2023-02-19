3289740f09daff0ecf21ff730e07fe3e74e71bd9
Sharon-Accuhit <sharon.chen@accuhit.net>
2023-02-19 18:00:44 +0800

    修改 app/set.sh 加入 npm 的軟連結

--- app/set.sh
+++ app/set.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #create Symbolic link
 ln -s /root/.nvm/versions/node/v16.18.1/bin/node /usr/local/bin/node
-echo "AccuHit IT !"
+ln -s /root/.nvm/versions/node/v16.18.1/bin/npm /usr/local/bin/npm
